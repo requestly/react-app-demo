c6822c012f11aeb8a2c17a1660fb3c85bee4fcc0
RuntimeTerror10 <parthb.git@gmail.com>
2022-11-03 18:42:54 +0530

    fix build errors

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,19 +1,15 @@
 export const ProductCard = ({ product }) => {
   return (
     <div className="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-700 dark:border-gray-700">
-      <a href="#">
-        <img
-          className="p-3 rounded-t-lg"
-          src={product.image}
-          alt="product image"
-        />
-      </a>
+      <img
+        className="p-3 rounded-t-lg"
+        src={product.image}
+        alt="product preview"
+      />
       <div className="px-5 pb-5">
-        <a href="#">
-          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-            {product.title}
-          </h5>
-        </a>
+        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          {product.title}
+        </h5>
         <div className="flex items-center mt-2.5 mb-5">
           <svg
             aria-hidden="true"
@@ -73,12 +69,9 @@ export const ProductCard = ({ product }) => {
           <span className="text-3xl font-bold text-gray-900 dark:text-white">
             ${product.price}
           </span>
-          <a
-            href="#"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
+          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Add to cart
-          </a>
+          </button>
         </div>
       </div>
     </div>
